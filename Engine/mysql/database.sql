5ed6c330a8a1551304e694bbac209c358f247504
Marijana123 <marijanaaastojanovic@gmail.com>
2022-12-15 12:38:11 +0100

    changes

--- Engine/mysql/database.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE students(
-    StudentID int not null AUTO_INCREMENT,
-    FirstName varchar(100) not null,
-    Surname varchar(100) not null,
-    PRIMARY KEY (StudentID)
-);
-
-INSERT INTO students(FirstName, Surname)
-VALUES("Pera", "Peric"), ("Sara", "Saric");
