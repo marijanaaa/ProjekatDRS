23b4a1ec63122dedececcaabb1a6e8cd8aa85943
Nikolina Aleksić <aleksic.pr31.2019@uns.ac.rs>
2022-12-23 21:15:25 +0100

    Changed account balance

--- UI/src/pages/AccountBalancePage.js
+++ UI/src/pages/AccountBalancePage.js
@@ -1,19 +1,31 @@
-import React, {
-    useState
-} from "react";
-import Card from '../components/component/card/Card';
+import React from "react";
+import classes from "../components/component/forms/LoginForm.module.css";
+import Card from "../components/component/card/Card";
 
-function AccountBalancePage()
-{
-    //pozvati get zahtev i cuvati u promenljivoj
-    const [stateInDolars, setStateInDolars] = useState(0);
-    setStateInDolars(1000)
-    return(
-        <Card>
-            <h1>Account Balance</h1>
-            <div>Balance in dolars {stateInDolars}</div>
-
-        </Card>
+function AccountBalancePage() {
+    //get
+    //
+    //let vr = data.ballanceInDollars
+    //setAccountBalance(1000);
+    //console.log(accountBalance)
+    //lista sa map
+    let accountBalance = 0
+    return (
+        <>
+            <h1>&nbsp;</h1>
+            <Card className={classes.account}>
+                <div>
+                    <h1>Account balance</h1>
+                    <label>Dollars: {accountBalance}$</label>
+                    <h1>&nbsp;</h1>
+                    <label>Kripto valute:</label>
+                    <ul>
+                        <li>Valuta1:</li>
+                    </ul>
+                </div>
+            </Card>
+        </>
     );
 }
+
 export default AccountBalancePage;
