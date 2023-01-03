1d8f188908002f84ca5f03f2624ed075c79874e1
Nela Jović <nelanekijovic@gmail.com>
2023-01-03 21:13:58 +0100

    Azuriranje stanje korisnika

--- engine/transaction_class.py
+++ engine/transaction_class.py
@@ -1,5 +1,5 @@
 from enums import transaction_state
-from app import transactionCollection,create_hash,userCollection
+from app import transactionCollection,create_hash,userCollection,cryptocurrencyCollection,update_cryptocurrency,decrease_crypto,increase_crypto
 import time
 from datetime import datetime
 
@@ -17,4 +17,8 @@ def transaction_processing(parametrs):
     hash = create_hash(parametrs["sender_email"], parametrs["receiver_email"], parametrs["amount"])
     transactionCollection.insert_one({'hash':hash,'sender':parametrs["sender_email"],'receiver':parametrs["receiver_email"],
                                'cryptocurrency':parametrs["cryptocurrency"],'amount':parametrs["amount"],'state': transaction_state.PROCESSED,'date':datetime.now()})
+    
+    #obj = {'email':parametrs["receiver_email"],'dollars':0,'BTC':0,'ETH':0,'USDT':0,'BUSD':0,'DOGE':0}
+    update_cryptocurrency(parametrs["sender_email"],parametrs["cryptocurrency"],decrease_crypto(parametrs["sender_email"],parametrs["cryptocurrency"],parametrs["amount"]))
+    update_cryptocurrency(parametrs["receiver_email"],parametrs["cryptocurrency"],increase_crypto(parametrs["receiver_email"],parametrs["cryptocurrency"],parametrs["amount"]))
     return True
